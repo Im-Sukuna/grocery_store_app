60bbbbbcd8ce56f0821b2d7c7517746af457cd38
Abraham Oj <abrahamojo14@gmail.com>
2024-04-05 22:08:29 +0100

    work on the first screen
    started working on the first screen

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'components/grocery_item_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 48,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text('Good Morning'),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                'Let\'s order fresh items for you',
+                style: GoogleFonts.lora(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Divider(),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+             const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                'Fresh Items',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2
+                ),
+                itemBuilder: (context, index) {
+                  return const GroceryItemTile();
+                }
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'home_page.dart';
@@ -37,7 +36,7 @@ class IntroPages extends StatelessWidget {
             context,
             MaterialPageRoute(
               builder: (context) {
-                return HomePage();
+                return const HomePage();
               },
             ),
           ),
